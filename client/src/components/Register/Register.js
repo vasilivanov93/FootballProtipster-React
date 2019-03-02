a0ffac2ccb908e008d563ecaf22ec772d9449e0d
Vasil Ivanov <vasilivanov93@gmail.com>
2019-03-02 09:28:32 +0200

    Add register page.

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -5,7 +5,48 @@ class Register extends Component {
     render() {
         return (
             <div className="Register">
+                <form className="form">
+                    <div className="form__inner">
+                        <div className="form__head">
+                            <h2 className="form__title">Register</h2>
+                        </div>
 
+                        <div className="form__body">
+                            <div className="form__controls">
+                                <label className="user" htmlFor="username">
+                                    <span className="hidden">Username</span>
+                                </label>
+
+                                <input type="text" id="username" className="field" name="username" placeholder="Username" required />
+                            </div>
+
+                            <div className="form__controls">
+                                <label className="envelope" htmlFor="email"><span className="hidden">Email</span></label>
+
+                                <input type="email" id="email" className="field" name="email"
+                                       placeholder="Email" required />
+                            </div>
+
+                            <div className="form__controls">
+                                <label className="lock" htmlFor="password"><span className="hidden">Password</span></label>
+
+                                <input type="password" id="password" className="field" name="password"
+                                       placeholder="Password" required />
+                            </div>
+
+                            <div className="form__controls">
+                                <label className="lock" htmlFor="repeatPassword"><span className="hidden">Repeat Password</span></label>
+
+                                <input type="password" id="repeatPassword" className="field" name="repeatPassword"
+                                       placeholder="Repeat Password" required />
+                            </div>
+                        </div>
+
+                        <div className="form__actions">
+                            <input type="submit" value="Register" name="register" className="btn btn--register" />
+                        </div>
+                    </div>
+                </form>
             </div>
         );
     }
